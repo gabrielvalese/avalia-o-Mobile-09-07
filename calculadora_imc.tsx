4eca368c9ca60d685e8a859f606eadb8f8477ef4
Crstt-pjl <santanacristhian56@gmail.com>
2026-07-09 08:14:14 -0300

    Add files via upload

---
+++ calculadora_imc.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Stack } from "expo-router";
+
+export default function CalculadoraIMC() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#e6f2e6",
+  },
+});
